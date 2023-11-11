2363712728fd7817c063cd94a64059253a595b71
Logan Losee <logan.losee99@gmail.com>
2023-11-11 13:36:12 -0700

    messing with routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,15 +3,15 @@ const path = require('path');
 
 module.exports = (app) => {
   // GET /api/notes - Read and return all saved notes as JSON
-  app.get('/api/notes', (req, res) => {
-    const data = JSON.parse(fs.readFileSync(path.join(__dirname, '../db.json'));
+  app.get('/db/db.json', (req, res) => {
+    const data = JSON.parse(fs.readFileSync(path.join(__dirname, '../db.json')));
     res.json(data);
   });
 
   // POST /api/notes - Receive a new note, add it to the db.json file, and return the new note
-  app.post('/api/notes', (req, res) => {
+  app.post('/db/db.json', (req, res) => {
     const newNote = req.body;
-    const data = JSON.parse(fs.readFileSync(path.join(__dirname, '../db.json'));
+    const data = JSON.parse(fs.readFileSync(path.join(__dirname, '../db.json')));
 
     // Generate a unique ID for the new note (you can use npm packages like 'uuid' or simply use a timestamp)
     newNote.id = Date.now();
--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -6,9 +9,42 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-require('./routes/apiRoutes')(app);
-require('./routes/htmlRoutes')(app);
+// API routes
+app.get('/db/db.json', (req, res) => {
+  // Read the notes from the db.json file and return them as JSON
+  const notes = JSON.parse(fs.readFileSync(path.join(__dirname, 'db.json')));
+  res.json(notes);
+});
 
+app.post('/db/db.json', (req, res) => {
+  const newNote = req.body;
+  const notes = JSON.parse(fs.readFileSync(path.join(__dirname, 'db.json')));
+
+  // Assign a unique ID to the new note (e.g., using a timestamp)
+  newNote.id = Date.now();
+
+  // Add the new note to the array
+  notes.push(newNote);
+
+  // Write the updated notes back to the db.json file
+  fs.writeFileSync(path.join(__dirname, 'db.json'), JSON.stringify(notes, null, 2));
+
+  // Send the new note as a response
+  res.json(newNote);
+});
+
+// HTML routes
+app.get('/notes', (req, res) => {
+  // Return the notes.html file
+  res.sendFile(path.join(__dirname, 'public', 'notes.html'));
+});
+
+app.get('*', (req, res) => {
+  // Return the index.html file for all other routes (default)
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
